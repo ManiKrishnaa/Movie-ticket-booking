1f5de0805637a7a9f5d047235be79692ca2b99d4
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-07-17 13:25:44 +0530

    Update Navbar.js

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -17,7 +17,7 @@ function Navbar({ isLoggedIn, handleLogout }) {
 
   const fetchLocations = async () => {
     try {
-      const response = await axios.get('http://movie-api-rosy.vercel.app/cities');
+      const response = await axios.get('https://movie-api-rosy.vercel.app/cities');
       setLocations(response.data.cities);
     } catch (error) {
       console.error('Error fetching locations:', error);
