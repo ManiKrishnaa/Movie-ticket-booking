f97aa092b6c389795a7b9ee15846e42a4c790a38
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-07-17 13:22:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const secretKey = "Movie_booking00124123124";
 
 app.use(cors(
     {
-        origin : {"https://movie-ticket-booking-eight.vercel.app"},
+        origin : {"http://movie-ticket-booking-eight.vercel.app"},
         methods : {"POST","GET"},
         credintials : true
     }
