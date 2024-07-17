08d40ad8e75f2f758170075cf52cff1c611ae730
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-07-17 15:16:43 +0530

    Update Movies.js

--- backend/models/Movies.js
+++ backend/models/Movies.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-    title: {type:String,required:true},
-    description: {type:String,required:true},
-    genre: {type:String,required:true},
-    releaseDate: {type:Date,required:true},
-    duration: {type:Number,required:true},
-    city : {type:String,required:true},
-    theaters :{type:String,required:true},
-    posterUrl: {type:String,required:true},
+    title: {type:String},
+    description: {type:String},
+    genre: {type:String},
+    releaseDate: {type:Date},
+    duration: {type:Number},
+    city : {type:String},
+    theaters :{type:String},
+    posterUrl: {type:String},
 });
 
 module.exports = mongoose.model('Movies',movieSchema)
