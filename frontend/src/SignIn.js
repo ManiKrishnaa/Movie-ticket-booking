0cf385fedf38227a912cbfe6866c98ab3d6dea63
ManiKrishnaa <130267764+ManiKrishnaa@users.noreply.github.com>
2024-09-12 15:09:10 +0530

    Update SignIn.js

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -11,7 +11,7 @@ function SignIn({handleLogin}) {
         e.preventDefault();
         
         try {
-            const response = await fetch('https://movie-ticket-booking-gold.vercel.app/signin', {
+            const response = await fetch('https://movie-ticket-booking-s1mx.onrender.com/signin', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
